1ebdbbc7c2e7fa20aa7b38117691d63eccf0652b
kimsundner <44000842+kimsundner@users.noreply.github.com>
2019-09-08 17:10:29 +0200

    playing around

--- camera/clmtrackr/emotion/script.js
+++ camera/clmtrackr/emotion/script.js
@@ -8,86 +8,89 @@ const classifier = new emotionClassifier();
 startCamera();
 
 cameraEl.addEventListener('play', () => {
-  // Resize everything to match the actual
-  // video frame size
-  canvasEl.width = cameraEl.videoWidth;
-  canvasEl.height = cameraEl.videoHeight;
-  cameraEl.width = cameraEl.videoWidth;
-  cameraEl.height = cameraEl.videoHeight;
+    // Resize everything to match the actual
+    // video frame size
+    canvasEl.width = cameraEl.videoWidth;
+    canvasEl.height = cameraEl.videoHeight;
+    cameraEl.width = cameraEl.videoWidth;
+    cameraEl.height = cameraEl.videoHeight;
 
-  // Set up classifier & model
-  pModel.shapeModel.nonRegularizedVectors.push(9);
-  pModel.shapeModel.nonRegularizedVectors.push(11);
-  classifier.init(emotionModel);
+    // Set up classifier & model
+    pModel.shapeModel.nonRegularizedVectors.push(9);
+    pModel.shapeModel.nonRegularizedVectors.push(11);
+    classifier.init(emotionModel);
 
-  // Set up tracker
-  tracker.init(pModel);
-  tracker.start(cameraEl);
+    // Set up tracker
+    tracker.init(pModel);
+    tracker.start(cameraEl);
 
-  // Start monitoring frames
-  window.requestAnimationFrame(renderFrame);  
+    // Start monitoring frames
+    window.requestAnimationFrame(renderFrame);
 });
 
 function renderFrame() {
-  var c = canvasEl.getContext('2d');
-  var p = tracker.getCurrentPosition();
-  if (!p) {
-    window.requestAnimationFrame(renderFrame);  
-    return; // Tracker not tracking yet
-  }
+    var c = canvasEl.getContext('2d');
+    var p = tracker.getCurrentPosition();
+    if (!p) {
+        window.requestAnimationFrame(renderFrame);
+        return; // Tracker not tracking yet
+    }
 
-  // Optional visual feedback of tracker
-  c.drawImage(cameraEl, 0, 0);
-  tracker.draw(canvasEl);
+    // Optional visual feedback of tracker
+    c.drawImage(cameraEl, 0, 0);
+    tracker.draw(canvasEl);
 
-  var cp = tracker.getCurrentParameters();
-  var er = classifier.meanPredict(cp);
-  if (er) {
-    updateData(er);
-  }
+    var cp = tracker.getCurrentParameters();
+    var er = classifier.meanPredict(cp);
+    if (er) {
+        updateData(er);
+    }
 
-  // Repeat!
-  window.requestAnimationFrame(renderFrame);
+    // Repeat!
+    window.requestAnimationFrame(renderFrame);
 }
 
 function updateData(er) {
-  var r = "";
-  for (var i=0;i<er.length;i++) {
-    // Simplify to an integer
-    er[i].value = parseInt(er[i].value * 100);
+    var r = "";
+    for (var i = 0; i < er.length; i++) {
+        // Simplify to an integer
+        er[i].value = parseInt(er[i].value * 100);
 
-    // Construct some HTML to show results
-    r+='<span class="result"><span class="label';
-    if (er[i].value > 50) r+= ' highlight';
-    r += '">' + er[i].emotion + '</span> <span class="value">' + er[i].value + '</span></span>';
-  }
-  resultsEl.innerHTML = r;
+        // Construct some HTML to show results
+        r += '<span class="result"><span class="label';
+        if (er[i].value > 50) r += ' highlight';
+        r += '">' + er[i].emotion + '</span> <span class="value">' + er[i].value + '</span></span>';
+    }
+    resultsEl.innerHTML = r;
 }
 
 // ------------------------
 
 // Reports outcome of trying to get the camera ready
 function cameraReady(err) {
-  if (err) {
-    console.log("Camera not ready: " + err);
-    return;
-  }
-  console.log("Camera ready");
+    if (err) {
+        console.log("Camera not ready: " + err);
+        return;
+    }
+    console.log("Camera ready");
 }
 
 // Tries to get the camera ready, and begins streaming video to the cameraEl element.
 function startCamera() {
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  if (!navigator.getUserMedia) {
-    cameraReady("getUserMedia not supported");
-    return;
-  }
-  navigator.getUserMedia({video:true}, 
-    (stream) => {
-    cameraEl.src = window.URL.createObjectURL(stream);
-    cameraReady();
-  },
-  (error) => {
-    cameraNotReady(error);
-  });
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    if (!navigator.getUserMedia) {
+        cameraReady("getUserMedia not supported");
+        return;
+    }
+    navigator.getUserMedia({ video: true },
+        (stream) => {
+            cameraEl.srcObject = stream;
+            //DEPRECATED in chrome
+            // video.play();
+            // cameraEl.src = window.URL.createObjectURL(stream);
+            cameraReady();
+        },
+        (error) => {
+            cameraNotReady(error);
+        });
 }
--- camera/simple/script.js
+++ camera/simple/script.js
@@ -7,29 +7,29 @@ startCamera();
 
 // Demonstrates capturing a frame as an IMG element
 function captureToImg() {
-  const imagesEl = document.getElementById('captureImages');
-  const offscreenCanvasEl = document.getElementById('offscreenCanvas');
-  
-  // 1. First we have to capture to a hidden canvas
-  offscreenCanvasEl.width = cameraEl.videoWidth;
-  offscreenCanvasEl.height = cameraEl.videoHeight;
-  var c = offscreenCanvasEl.getContext('2d');
-  c.drawImage(cameraEl, 0, 0, cameraEl.videoWidth , cameraEl.videoHeight);
+    const imagesEl = document.getElementById('captureImages');
+    const offscreenCanvasEl = document.getElementById('offscreenCanvas');
 
-  // 2. Then we grab the data from the hidden canvas, and set it as the
-  // source of a new IMG element
-  var img = document.createElement('img');
-  img.src = offscreenCanvasEl.toDataURL('image/jpeg');
-  imagesEl.appendChild(img); // Add it to a DIV
+    // 1. First we have to capture to a hidden canvas
+    offscreenCanvasEl.width = cameraEl.videoWidth;
+    offscreenCanvasEl.height = cameraEl.videoHeight;
+    var c = offscreenCanvasEl.getContext('2d');
+    c.drawImage(cameraEl, 0, 0, cameraEl.videoWidth, cameraEl.videoHeight);
+
+    // 2. Then we grab the data from the hidden canvas, and set it as the
+    // source of a new IMG element
+    var img = document.createElement('img');
+    img.src = offscreenCanvasEl.toDataURL('image/jpeg');
+    imagesEl.appendChild(img); // Add it to a DIV
 }
 
 // Demonstrates drawing a frame to a canvas
 function captureToCanvas() {
-  const canvasEl = document.getElementById('captureCanvas');
-  canvasEl.width = cameraEl.videoWidth;
-  canvasEl.height = cameraEl.videoHeight;
-  var c = canvasEl.getContext('2d');
-  c.drawImage(cameraEl, 0, 0, cameraEl.videoWidth , cameraEl.videoHeight);
+    const canvasEl = document.getElementById('captureCanvas');
+    canvasEl.width = cameraEl.videoWidth;
+    canvasEl.height = cameraEl.videoHeight;
+    var c = canvasEl.getContext('2d');
+    c.drawImage(cameraEl, 0, 0, cameraEl.videoWidth, cameraEl.videoHeight);
 }
 
 // ------------------------
@@ -37,25 +37,28 @@ function captureToCanvas() {
 
 // Reports outcome of trying to get the camera ready
 function cameraReady(err) {
-  if (err) {
-    console.log('Camera not ready: ' + err);
-    return;
-  }
+    if (err) {
+        console.log('Camera not ready: ' + err);
+        return;
+    }
 }
 
 // Tries to get the camera ready, and begins streaming video to the cameraEl element.
 function startCamera() {
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-  if (!navigator.getUserMedia) {
-    cameraReady('getUserMedia not supported');
-    return;
-  }
-  navigator.getUserMedia({video:true}, 
-    (stream) => {
-      cameraEl.src = window.URL.createObjectURL(stream);
-      cameraReady();
-    },
-    (error) => {
-      cameraReady(error);
-    });
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    if (!navigator.getUserMedia) {
+        cameraReady('getUserMedia not supported');
+        return;
+    }
+    navigator.getUserMedia({ video: true },
+        (stream) => {
+            cameraEl.srcObject = stream;
+            //DEPRECATED in chrome
+            // video.play();
+            // cameraEl.src = window.URL.createObjectURL(stream);
+            cameraReady();
+        },
+        (error) => {
+            cameraReady(error);
+        });
 }
